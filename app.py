4d8e253d13f2a286a42c904abe4e10db5e605dae
TivonB <112200765+TivonB@users.noreply.github.com>
2022-09-08 14:32:24 -0400

    Add files via upload

---
+++ app.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep  8 14:06:02 2022
+
+@author: tivon_37vlsu0
+"""
+
+import streamlit as st
+
+#Find more emojis here: https://www.webfx.com/tools/emoji-cheat-sheet/
+st.set_page_config(page_title="My Webpage", page_icon=":tada:",layout="wide")
+
+#---- HEADER SECTION ----
+st.subheader("This is the subheader.")
+st.title("This the Title location")
+st.write("This is a test app")
+st.write("This app is used to understand streamlit")
